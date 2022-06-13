328757ad56b12b4a42b27d64535d3f78cff6647e
Robert Wilkins III <1147229+genome21@users.noreply.github.com>
2022-06-13 23:33:51 +0000

    fixed deployment and test scripts

--- main.tf
+++ main.tf
@@ -13,3 +13,25 @@ variable "region" {
 variable "zone" {
   type = string
 }
+
+variable "basename" {
+    type = string
+}
+
+variable "nodes" {
+    type = number
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name = "rwtest"
+    machine_type = "f1-micro"
+    zone = "us-central1-a"
+    boot_disk {
+    initialize_params {
+        image = "centos-cloud/centos-7"
+    }
+}
+    network_interface {
+        subnetwork = "default"
+    }
+}
